749f3800329db96e55229867971214ef4e720d57
Carlos Navarro <carlos.navarro@ylconsulting.com>
2020-03-10 13:29:29 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { DocumentComponent } from './document/document.component';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PdfViewerModule, LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService,
+  ToolbarService, NavigationService, TextSearchService, TextSelectionService, PrintService  } from '@syncfusion/ej2-angular-pdfviewer';
+
+import { ToolbarModule } from '@syncfusion/ej2-angular-navigations';
+
+
+// import the PdfViewer Module for the PDF Viewer component
+// import { PdfViewerComponent } from '@syncfusion/ej2-angular-pdfviewer';
+import { AppComponent }  from './app.component';
+
+const appRoutes: Routes = [
+  { path: 'document', component: DocumentComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PdfViewerModule,
+    ToolbarModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
   ],
-  providers: [],
+  providers: [LinkAnnotationService, BookmarkViewService, MagnificationService,
+    ThumbnailViewService, ToolbarService, NavigationService, TextSearchService, TextSelectionService, PrintService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
